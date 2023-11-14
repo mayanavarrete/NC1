2506609bd627d9b694cd4c2f4ec3ad8e6dc48f8b
Maya Navarrete Moncada <>
2023-11-14 12:19:17 +0100

    first commit

---
+++ Apple Music App/Apple_Music_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_Music_AppApp.swift
+//  Apple Music App
+//
+//  Created by Maya Navarrete Moncada on 14/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Apple_Music_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
